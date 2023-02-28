d1bbfefbb20630ee14a0e0ff532090806b7b12a0
T3admin <126587614+T3admin@users.noreply.github.com>
2023-02-28 15:09:15 +0000

    Update streamlit_app.py
    additional lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 Import Streamlit
 
-Streamlit.title(My Parents New Healthy Diner)
+Streamlit.title('My Parents New Healthy Diner')
+
+Streamlit.header('Breakfast Menu')
+Streamlit.text('Omega 3 & Blueberry Oatmeal')
+Streamlit.text('Kale, Spinach & Rocket Smoothie')
+Streamlit.text('Hard Boiled Free-Range Egg')
